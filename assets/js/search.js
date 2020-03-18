8dc3e111714828798d221672f30c45501aa98882
AndresCalimero <AndresCalimero@users.noreply.github.com>
2020-03-18 20:21:15 +0000

    deploy: f679d3441c2000eda90712827783f2ad684a33b3

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"Module","128":"Class","2048":"Method"},"rows":[{"id":0,"kind":1,"name":"\"HelloLib\"","url":"modules/_hellolib_.html","classes":"tsd-kind-module"},{"id":1,"kind":128,"name":"HelloLib","url":"classes/_hellolib_.hellolib.html","classes":"tsd-kind-class tsd-parent-kind-module","parent":"\"HelloLib\""},{"id":2,"kind":2048,"name":"sayHello","url":"classes/_hellolib_.hellolib.html#sayhello","classes":"tsd-kind-method tsd-parent-kind-class","parent":"\"HelloLib\".HelloLib"}]};
